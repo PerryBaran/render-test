45a5867e7c1a56db9cf87a59d3d6c814dedd00a6
Perry Baran <perrybaran@gmail.com>
2022-12-20 14:32:02 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import axios from "axios";
+
+const userLogin = async (data) => {
+  try {
+    const response = await axios.post(`https://soundcloud-clone-api.onrender.com/users/login`, {
+      email: "jenico.producer@gmail.com",
+      password: "pleaseWork"
+  }, {
+      withCredentials: true,
+    });
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+}
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={userLogin} >Login</button>
     </div>
   );
 }
